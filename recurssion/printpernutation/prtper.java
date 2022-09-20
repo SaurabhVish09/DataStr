5b2bb7c4ab2a8d9d2f150e5b28edc1078d18aa7b
SaurabhVish09 <ssv6392@gmail.com>
2022-09-20 17:08:29 +0530

    print permutation completed

---
+++ recurssion/printpernutation/prtper.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.*;
+
+public class prtper {
+
+    public static void main(String[] args) throws Exception {
+        Scanner scn = new Scanner(System.in);
+        String str = scn.next();
+        printPermutations(str,"");
+
+    }
+
+    public static void printPermutations(String str, String asf) { 
+        if(str.length()==0){
+            System.out.println(asf);
+            return;
+        }
+        for(int i  =0 ; i < str.length();i++){
+            char ch = str.charAt(i);
+            String rq = str.substring(0,i);
+            String lq = str.substring(i+1);
+            String whole = rq+lq;
+            printPermutations(whole,asf+ch);
+        }
+        
+    }
+
+}
